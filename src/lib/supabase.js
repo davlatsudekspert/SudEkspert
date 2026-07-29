802b7b0879b80656af86bd1dc4c7cb2fa0dcdc56
Ali <davlatsudekspert@gmail.com>
2026-07-29 22:01:09 +0500

    alsi

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://hnxqonffsajpmqfwpxex.supabase.co";
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhueHFvbmZmc2FqcG1xZndweGV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxNjI5OTUsImV4cCI6MjEwMDczODk5NX0.C_5Qf2GodcfPTpwAhH1BwyDYqEIFr6OybWJYhwsv5yo";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
